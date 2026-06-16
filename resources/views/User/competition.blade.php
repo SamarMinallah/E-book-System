4aa745bd141f2b583405f6a80f922865c3b48131
wajihanoor125 <wajihawajid2121@gmail.com>
2026-06-16 15:26:53 +0500

    competition changes

--- resources/views/User/competition.blade.php
+++ resources/views/User/competition.blade.php
@@ -8,7 +8,8 @@
  
     <link rel="stylesheet" href="/css/competition.css">
 
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 <section class="competition-hero">
 
@@ -73,20 +74,29 @@
 
     <div class="prize-grid">
 
+   @foreach($allcomp as $comp)
         <div class="prize-card gold">
-            <h3>🥇 First Place</h3>
+            <h2>{{$comp->title}}</h2>
+           <div class="row">
+            <div class="col-md-4">
+                 <h3>🥇 First Place</h3>
             <span>{{$comp->prize1}}</span>
-        </div>
-
-        <div class="prize-card silver">
-            <h3>🥈 Second Place</h3>
+            </div>
+            <div class="col-md-4">
+                  <h3>🥇 Second Place</h3>
             <span>{{$comp->prize2}}</span>
-        </div>
-
-        <div class="prize-card bronze">
-            <h3>🥉 Third Place</h3>
+            </div>
+            <div class="col-md-4">
+                  <h3>🥇 Third Place</h3>
             <span>{{$comp->prize3}}</span>
+            </div>
+           </div>
         </div>
+      @endforeach
+
+        
+
+       
 
     </div>
 
